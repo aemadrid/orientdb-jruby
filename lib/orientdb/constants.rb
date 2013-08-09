965147e67ed0f5b06d30ce7f61005d4a560aeb47
Glenn Goodrich <glenn.goodrich@gmail.com>
2013-08-09 11:43:31 -0400

    add constant for gremlin

--- lib/orientdb/constants.rb
+++ lib/orientdb/constants.rb
@@ -27,6 +27,9 @@ module OrientDB
   #Blueprints
   BLUEPRINTS = com.tinkerpop.blueprints
 
+  #Gremlin
+  Gremlin = com.tinkerpop.gremlin
+
   OrientGraph = BLUEPRINTS.impls.orient.OrientGraph
   Conclusion = com.tinkerpop.blueprints.TransactionalGraph::Conclusion
 
